8f672731411d0deffbf6a08f1badf637d29c6664
青木伸子 <aokinobuko@aokinobuko-no-MacBook-Air.local>
2017-08-18 17:30:50 +0900

    nicknameを追加した

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170817164039) do
+ActiveRecord::Schema.define(version: 20170818081134) do
 
   create_table "tweets", force: :cascade do |t|
     t.text     "tweet",      limit: 65535
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20170817164039) do
     t.string   "unconfirmed_email",      limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
+    t.string   "nickname",               limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
