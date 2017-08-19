000f44e62c248fdbe62dca419105b73826854dae
青木伸子 <>
2017-08-19 14:11:55 +0900

    herokuに関する設定

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,8 @@ module Sakelog
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :ja
 
+    config.assets.initialize_on_precompile = false
+
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
