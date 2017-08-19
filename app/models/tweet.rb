0812317c18bea4beee69cdcf3f5b2aae7514fcdf
青木伸子 <aokinobuko@aokinobuko-no-MacBook-Air.local>
2017-08-19 20:04:26 +0900

    validatesも追加

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,5 +3,7 @@ class Tweet < ActiveRecord::Base
   belongs_to :user
   mount_uploader :img, ImgUploader
 
-  #validates_presence_of :text, :img
+  validates :text, :presence => {:message =>'は必須です'}
+  validates :img, :presence => {:message =>'は必須です'}
+
 end
